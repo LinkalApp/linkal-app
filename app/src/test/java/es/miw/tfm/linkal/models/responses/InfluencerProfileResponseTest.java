2cd3eed2904289eb38d2d519404d7cfc6da50790
irenerlz <irenereciolopez12@gmail.com>
2026-05-24 10:10:59 +0200

    Added tests for my influencer profile LinkalApp/linkal#21

--- app/src/test/java/es/miw/tfm/linkal/models/responses/InfluencerProfileResponseTest.java
+++ app/src/test/java/es/miw/tfm/linkal/models/responses/InfluencerProfileResponseTest.java
@@ -74,6 +74,47 @@ public class InfluencerProfileResponseTest {
     }
 
     @Test
+    public void getVerified_returnsTrue() {
+        InfluencerProfileResponse r = new InfluencerProfileResponse();
+        r.setVerified(true);
+        assertTrue(r.getVerified());
+    }
+
+    @Test
+    public void getVerified_returnsFalse() {
+        InfluencerProfileResponse r = new InfluencerProfileResponse();
+        r.setVerified(false);
+        assertFalse(r.getVerified());
+    }
+
+    @Test
+    public void getAverageRating_returnsCorrectValue() {
+        InfluencerProfileResponse r = new InfluencerProfileResponse();
+        r.setAverageRating(4.2);
+        assertEquals(4.2, r.getAverageRating(), 0.001);
+    }
+
+    @Test
+    public void getAverageRating_returnsNullByDefault() {
+        InfluencerProfileResponse r = new InfluencerProfileResponse();
+        assertNull(r.getAverageRating());
+    }
+
+    @Test
+    public void getAverageRating_returnsPerfectScore() {
+        InfluencerProfileResponse r = new InfluencerProfileResponse();
+        r.setAverageRating(5.0);
+        assertEquals(5.0, r.getAverageRating(), 0.001);
+    }
+
+    @Test
+    public void getAverageRating_returnsMinimumScore() {
+        InfluencerProfileResponse r = new InfluencerProfileResponse();
+        r.setAverageRating(1.0);
+        assertEquals(1.0, r.getAverageRating(), 0.001);
+    }
+
+    @Test
     public void defaultConstructor_allFieldsAreNull() {
         InfluencerProfileResponse r = new InfluencerProfileResponse();
         assertNull(r.getName());
@@ -85,5 +126,7 @@ public class InfluencerProfileResponseTest {
         assertNull(r.getInstagram());
         assertNull(r.getTiktok());
         assertNull(r.getYoutube());
+        assertNull(r.getVerified());
+        assertNull(r.getAverageRating());
     }
 }
